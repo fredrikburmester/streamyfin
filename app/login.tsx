1368fbd9358782018e275897e901c28721b0c627
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-12 18:00:25 +0200

    fix: allow login without password

--- app/login.tsx
+++ app/login.tsx
@@ -11,7 +11,6 @@ import { z } from "zod";
 
 const CredentialsSchema = z.object({
   username: z.string().min(1, "Username is required"),
-  password: z.string().min(1, "Password is required"),
 });
 
 const Login: React.FC = () => {
