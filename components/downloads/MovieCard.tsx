1214d0763eef591d11f505ecc45c95fd14bf3fb6
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-13 16:00:39 +0200

    chore: todo

--- components/downloads/MovieCard.tsx
+++ components/downloads/MovieCard.tsx
@@ -9,10 +9,22 @@ import { useCallback } from "react";
 import * as Haptics from "expo-haptics";
 import { useAtom } from "jotai";
 import { currentlyPlayingItemAtom } from "../CurrentlyPlayingBar";
+import { useQuery } from "@tanstack/react-query";
 
 export const MovieCard: React.FC<{ item: BaseItemDto }> = ({ item }) => {
   const { deleteFile } = useFiles();
   const [_, setCp] = useAtom(currentlyPlayingItemAtom);
+
+  // const fetchFileSize = async () => {
+  //   const filePath = `${FileSystem.documentDirectory}/${item.Id}.mp4`;
+  //   const info = await FileSystem.getInfoAsync(filePath);
+  //   return info.exists ? info.size : null;
+  // };
+
+  // const { data: fileSize } = useQuery({
+  //   queryKey: ["fileSize", item?.Id],
+  //   queryFn: fetchFileSize,
+  // });
 
   const openFile = useCallback(() => {
     setCp({
@@ -41,11 +53,17 @@ export const MovieCard: React.FC<{ item: BaseItemDto }> = ({ item }) => {
             className="bg-neutral-900 border border-neutral-800 rounded-2xl p-4"
           >
             <Text className=" font-bold">{item.Name}</Text>
-            <View className="flex flex-row items-center justify-between">
+            <View className="flex flex-col">
               <Text className=" text-xs opacity-50">{item.ProductionYear}</Text>
               <Text className=" text-xs opacity-50">
                 {runtimeTicksToMinutes(item.RunTimeTicks)}
               </Text>
+              {/* <Text className=" text-xs opacity-50">
+                Size:{" "}
+                {fileSize
+                  ? `${(fileSize / 1000000).toFixed(0)} MB`
+                  : "Calculating..."}{" "}
+                  </Text>*/}
             </View>
           </TouchableOpacity>
         </ContextMenu.Trigger>
