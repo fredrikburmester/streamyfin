a80e065cdb0634ba1f940ddb12fb3695cf42d841
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-14 08:13:14 +0200

    fix: formatting of specials season number

--- components/ItemCardText.tsx
+++ components/ItemCardText.tsx
@@ -7,6 +7,17 @@ type ItemCardProps = {
   item: BaseItemDto;
 };
 
+function seasonNameToIndex(seasonName: string | null | undefined) {
+  if (!seasonName) return -1;
+  if (seasonName.startsWith("Season")) {
+    return parseInt(seasonName.replace("Season ", ""));
+  }
+  if (seasonName.startsWith("Specials")) {
+    return 0;
+  }
+  return -1;
+}
+
 export const ItemCardText: React.FC<ItemCardProps> = ({ item }) => {
   return (
     <View className="mt-2 flex flex-col grow-0">
@@ -17,9 +28,8 @@ export const ItemCardText: React.FC<ItemCardProps> = ({ item }) => {
             style={{ flexWrap: "wrap" }}
             className="flex text-xs opacity-50 break-all"
           >
-            {`S${item.SeasonName?.replace(
-              "Season ",
-              ""
+            {`S${seasonNameToIndex(
+              item?.SeasonName,
             )}:E${item.IndexNumber?.toString()}`}{" "}
             {item.Name}
           </Text>
