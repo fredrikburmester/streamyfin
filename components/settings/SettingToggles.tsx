aa0fbd6fad59a93bb2aa5552b55f5c81a13fa618
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-15 16:46:50 +0200

    fix: refactor

--- components/settings/SettingToggles.tsx
+++ components/settings/SettingToggles.tsx
@@ -1,4 +1,4 @@
-import { Switch, View } from "react-native";
+import { Linking, Switch, TouchableOpacity, View } from "react-native";
 import { Text } from "../common/Text";
 import { useAtom } from "jotai";
 import { useSettings } from "@/utils/atoms/settings";
@@ -9,19 +9,50 @@ export const SettingToggles: React.FC = () => {
   return (
     <View className="flex flex-col rounded-xl mb-4 overflow-hidden border-neutral-800 divide-y-2 divide-solid divide-neutral-800 ">
       <View className="flex flex-row items-center justify-between bg-neutral-900 p-4">
-        <Text>Auto rotate</Text>
+        <View className="shrink">
+          <Text className="font-semibold">Auto rotate</Text>
+          <Text className="text-xs opacity-50">
+            Important on android since the video player orientation is locked to
+            the app orientation.
+          </Text>
+        </View>
         <Switch
           value={settings?.autoRotate}
           onValueChange={(value) => updateSettings({ autoRotate: value })}
         />
       </View>
       <View className="flex flex-row items-center justify-between bg-neutral-900 p-4">
-        <Text>Start videos in fullscreen</Text>
+        <View className="shrink">
+          <Text className="font-semibold">Start videos in fullscreen</Text>
+          <Text className="text-xs opacity-50">
+            Clicking a video will start it in fullscreen mode, instead of
+            inline.
+          </Text>
+        </View>
         <Switch
           value={settings?.openFullScreenVideoPlayerByDefault}
           onValueChange={(value) =>
             updateSettings({ openFullScreenVideoPlayerByDefault: value })
           }
+        />
+      </View>
+      <View className="flex flex-row items-center justify-between bg-neutral-900 p-4">
+        <View className="flex flex-col">
+          <Text className="font-semibold">Use popular lists plugin</Text>
+          <Text className="text-xs opacity-50">Made by: lostb1t</Text>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL(
+                "https://github.com/lostb1t/jellyfin-plugin-media-lists",
+              );
+            }}
+          >
+            <Text className="text-xs text-purple-600">More info</Text>
+          </TouchableOpacity>
+        </View>
+        <Switch
+          value={settings?.usePopularPlugin}
+          onValueChange={(value) => updateSettings({ usePopularPlugin: value })}
         />
       </View>
     </View>
