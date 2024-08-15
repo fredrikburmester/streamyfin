a5b88bba8508a9def62f25aa4af1accdca8c0730
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-15 10:37:26 +0200

    fix: link correctly

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: "Oops!" }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href={"/"} style={styles.link}>
+        <Link href={"/home"} style={styles.link}>
           <ThemedText type="link">Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
