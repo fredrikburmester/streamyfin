dec45056f3c8b18b7a4dae2bac32778e3c5e7ad1
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-18 11:53:40 +0200

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,20 @@
+import { CurrentlyPlayingBar } from "@/components/CurrentlyPlayingBar";
 import { JellyfinProvider } from "@/providers/JellyfinProvider";
+import { useSettings } from "@/utils/atoms/settings";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { Provider as JotaiProvider } from "jotai";
-import { useEffect, useRef, useState } from "react";
-import "react-native-reanimated";
-import * as ScreenOrientation from "expo-screen-orientation";
-import { StatusBar } from "expo-status-bar";
-import { CurrentlyPlayingBar } from "@/components/CurrentlyPlayingBar";
-import { ActionSheetProvider } from "@expo/react-native-action-sheet";
-import { useJobProcessor } from "@/utils/atoms/queue";
-import { JobQueueProvider } from "@/providers/JobQueueProvider";
 import { useKeepAwake } from "expo-keep-awake";
-import { useSettings } from "@/utils/atoms/settings";
+import { Stack } from "expo-router";
+import * as ScreenOrientation from "expo-screen-orientation";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { Provider as JotaiProvider } from "jotai";
+import { useEffect, useRef } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -78,98 +76,96 @@ function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <QueryClientProvider client={queryClientRef.current}>
-        <JobQueueProvider>
-          <ActionSheetProvider>
-            <BottomSheetModalProvider>
-              <JellyfinProvider>
-                <StatusBar style="light" backgroundColor="#000" />
-                <ThemeProvider value={DarkTheme}>
-                  <Stack initialRouteName="/home">
-                    <Stack.Screen
-                      name="(auth)/(tabs)"
-                      options={{
-                        headerShown: false,
-                        title: "",
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/settings"
-                      options={{
-                        headerShown: true,
-                        title: "Settings",
-                        headerStyle: { backgroundColor: "black" },
-                        headerShadowVisible: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/items/[id]"
-                      options={{
-                        title: "",
-                        headerShown: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/collections/[collectionId]"
-                      options={{
-                        title: "",
-                        headerShown: true,
-                        headerStyle: { backgroundColor: "black" },
-                        headerShadowVisible: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/artists/page"
-                      options={{
-                        title: "",
-                        headerShown: true,
-                        headerStyle: { backgroundColor: "black" },
-                        headerShadowVisible: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/artists/[artistId]/page"
-                      options={{
-                        title: "",
-                        headerShown: true,
-                        headerStyle: { backgroundColor: "black" },
-                        headerShadowVisible: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/albums/[albumId]"
-                      options={{
-                        title: "",
-                        headerShown: true,
-                        headerStyle: { backgroundColor: "black" },
-                        headerShadowVisible: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/songs/[songId]"
-                      options={{
-                        title: "",
-                        headerShown: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="(auth)/series/[id]"
-                      options={{
-                        title: "",
-                        headerShown: false,
-                      }}
-                    />
-                    <Stack.Screen
-                      name="login"
-                      options={{ headerShown: false, title: "Login" }}
-                    />
-                    <Stack.Screen name="+not-found" />
-                  </Stack>
-                  <CurrentlyPlayingBar />
-                </ThemeProvider>
-              </JellyfinProvider>
-            </BottomSheetModalProvider>
-          </ActionSheetProvider>
-        </JobQueueProvider>
+        <ActionSheetProvider>
+          <BottomSheetModalProvider>
+            <JellyfinProvider>
+              <StatusBar style="light" backgroundColor="#000" />
+              <ThemeProvider value={DarkTheme}>
+                <Stack initialRouteName="/home">
+                  <Stack.Screen
+                    name="(auth)/(tabs)"
+                    options={{
+                      headerShown: false,
+                      title: "",
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/settings"
+                    options={{
+                      headerShown: true,
+                      title: "Settings",
+                      headerStyle: { backgroundColor: "black" },
+                      headerShadowVisible: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/items/[id]"
+                    options={{
+                      title: "",
+                      headerShown: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/collections/[collectionId]"
+                    options={{
+                      title: "",
+                      headerShown: true,
+                      headerStyle: { backgroundColor: "black" },
+                      headerShadowVisible: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/artists/page"
+                    options={{
+                      title: "",
+                      headerShown: true,
+                      headerStyle: { backgroundColor: "black" },
+                      headerShadowVisible: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/artists/[artistId]/page"
+                    options={{
+                      title: "",
+                      headerShown: true,
+                      headerStyle: { backgroundColor: "black" },
+                      headerShadowVisible: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/albums/[albumId]"
+                    options={{
+                      title: "",
+                      headerShown: true,
+                      headerStyle: { backgroundColor: "black" },
+                      headerShadowVisible: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/songs/[songId]"
+                    options={{
+                      title: "",
+                      headerShown: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="(auth)/series/[id]"
+                    options={{
+                      title: "",
+                      headerShown: false,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="login"
+                    options={{ headerShown: false, title: "Login" }}
+                  />
+                  <Stack.Screen name="+not-found" />
+                </Stack>
+                <CurrentlyPlayingBar />
+              </ThemeProvider>
+            </JellyfinProvider>
+          </BottomSheetModalProvider>
+        </ActionSheetProvider>
       </QueryClientProvider>
     </GestureHandlerRootView>
   );
