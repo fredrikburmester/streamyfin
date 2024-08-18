5281cba2842a79807ffce8869b698436edb59819
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-18 10:52:20 +0200

    feat: inf horizontal scroll for media lists

--- components/medialists/MediaListSection.tsx
+++ components/medialists/MediaListSection.tsx
@@ -1,56 +1,72 @@
-import { View, ViewProps } from "react-native";
-import { Text } from "@/components/common/Text";
-import settings from "@/app/(auth)/settings";
-import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
-import { getItemsApi } from "@jellyfin/sdk/lib/utils/api";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { ScrollingCollectionList } from "../home/ScrollingCollectionList";
 import { apiAtom, userAtom } from "@/providers/JellyfinProvider";
-import { useSettings } from "@/utils/atoms/settings";
-import { useRouter } from "expo-router";
+import {
+  BaseItemDto,
+  BaseItemDtoQueryResult,
+} from "@jellyfin/sdk/lib/generated-client/models";
+import { getItemsApi } from "@jellyfin/sdk/lib/utils/api";
+import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
-import { useState } from "react";
+import { View, ViewProps } from "react-native";
+import { ScrollingCollectionList } from "../home/ScrollingCollectionList";
+import { Text } from "../common/Text";
+import { InfiniteHorizontalScroll } from "../common/InfiniteHorrizontalScroll";
+import { TouchableItemRouter } from "../common/TouchableItemRouter";
+import MoviePoster from "../posters/MoviePoster";
+import { useCallback } from "react";
 
 interface Props extends ViewProps {
   collection: BaseItemDto;
 }
 
 export const MediaListSection: React.FC<Props> = ({ collection, ...props }) => {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-
   const [api] = useAtom(apiAtom);
   const [user] = useAtom(userAtom);
 
-  const [loading, setLoading] = useState(false);
-  const [settings, _] = useSettings();
-
-  const { data: popularItems, isLoading: isLoadingPopular } = useQuery<
-    BaseItemDto[]
-  >({
-    queryKey: [collection.Id, user?.Id],
-    queryFn: async () => {
-      if (!api || !user?.Id || !collection.Id) return [];
+  const fetchItems = useCallback(
+    async ({
+      pageParam,
+    }: {
+      pageParam: number;
+    }): Promise<BaseItemDtoQueryResult | null> => {
+      if (!api || !user?.Id) return null;
 
       const response = await getItemsApi(api).getItems({
         userId: user.Id,
         parentId: collection.Id,
+        startIndex: pageParam,
         limit: 10,
       });
 
-      return response.data.Items || [];
+      return response.data;
     },
-    enabled: !!api && !!user?.Id && !!collection.Id,
-    staleTime: 0,
-  });
+    [api, user?.Id, collection.Id]
+  );
+
+  if (!collection) return null;
 
   return (
-    <ScrollingCollectionList
-      title={collection.Name || ""}
-      data={popularItems}
-      loading={isLoadingPopular}
-      disabled={!collection.Id}
-      {...props}
-    />
+    <View {...props}>
+      <Text className="px-4 text-2xl font-bold mb-2 text-neutral-100">
+        {collection.Name}
+      </Text>
+      <InfiniteHorizontalScroll
+        height={247}
+        renderItem={(item, index) => (
+          <TouchableItemRouter
+            key={index}
+            item={item}
+            className={`flex flex-col
+              ${"w-32"}
+            `}
+          >
+            <View>
+              <MoviePoster item={item} />
+            </View>
+          </TouchableItemRouter>
+        )}
+        queryFn={fetchItems}
+        queryKey={["media-list", collection.Id!]}
+      />
+    </View>
   );
 };
