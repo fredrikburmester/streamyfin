ea0681fd90a90d6c4d263a072fe7e683f5b4ef3d
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-09-03 18:53:48 +0300

    chore

--- utils/atoms/orientation.ts
+++ utils/atoms/orientation.ts
@@ -1,5 +1,4 @@
 import * as ScreenOrientation from "expo-screen-orientation";
-import { Orientation } from "expo-screen-orientation";
 import { atom } from "jotai";
 
 export const orientationAtom = atom<number>(
