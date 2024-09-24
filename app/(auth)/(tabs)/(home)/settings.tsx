cd6158e1411664393b9a9a2e10f859c12e6e62b3
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-09-24 20:04:50 +0200

    chore

--- app/(auth)/(tabs)/(home)/settings.tsx
+++ app/(auth)/(tabs)/(home)/settings.tsx
@@ -87,19 +87,17 @@ export default function settings() {
 
         <SettingToggles />
 
-        {/* <View>
+        <View>
           <Text className="font-bold text-lg mb-2">Tests</Text>
           <Button
             onPress={() => {
-              toast.success("Download started", {
-                invert: true,
-              });
+              toast.success("Download started");
             }}
             color="black"
           >
             Test toast
           </Button>
-        </View> */}
+        </View>
 
         <View>
           <Text className="font-bold text-lg mb-2">Account and storage</Text>
