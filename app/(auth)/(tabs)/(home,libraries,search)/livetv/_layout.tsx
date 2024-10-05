4faab81903592bb8b9008267e8d3632a2714df37
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-10-05 12:38:02 +0200

    chore

--- app/(auth)/(tabs)/(home,libraries,search)/livetv/_layout.tsx
+++ app/(auth)/(tabs)/(home,libraries,search)/livetv/_layout.tsx
@@ -4,7 +4,8 @@ import type {
   MaterialTopTabNavigationEventMap,
 } from "@react-navigation/material-top-tabs";
 import { ParamListBase, TabNavigationState } from "@react-navigation/native";
-import { withLayoutContext } from "expo-router";
+import { Stack, withLayoutContext } from "expo-router";
+import React from "react";
 
 const { Navigator } = createMaterialTopTabNavigator();
 
@@ -17,27 +18,31 @@ export const Tab = withLayoutContext<
 
 const Layout = () => {
   return (
-    <Tab
-      initialRouteName="programs"
-      keyboardDismissMode="none"
-      screenOptions={{
-        tabBarLabelStyle: { fontSize: 10 },
-        tabBarItemStyle: {
-          width: 100,
-        },
-        tabBarStyle: { backgroundColor: "black" },
-        animationEnabled: true,
-        lazy: true,
-        swipeEnabled: true,
-        tabBarIndicatorStyle: { backgroundColor: "#9334E9" },
-        tabBarScrollEnabled: true,
-      }}
-    >
-      <Tab.Screen name="programs" />
-      <Tab.Screen name="guide" />
-      <Tab.Screen name="channels" />
-      <Tab.Screen name="recordings" />
-    </Tab>
+    <>
+      <Stack.Screen options={{ title: "Live TV" }} />
+      <Tab
+        initialRouteName="programs"
+        keyboardDismissMode="none"
+        screenOptions={{
+          tabBarBounces: true,
+          tabBarLabelStyle: { fontSize: 10 },
+          tabBarItemStyle: {
+            width: 100,
+          },
+          tabBarStyle: { backgroundColor: "black" },
+          animationEnabled: true,
+          lazy: true,
+          swipeEnabled: true,
+          tabBarIndicatorStyle: { backgroundColor: "#9334E9" },
+          tabBarScrollEnabled: true,
+        }}
+      >
+        <Tab.Screen name="programs" />
+        <Tab.Screen name="guide" />
+        <Tab.Screen name="channels" />
+        <Tab.Screen name="recordings" />
+      </Tab>
+    </>
   );
 };
 
