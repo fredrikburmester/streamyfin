33b94105c2d26fd0ae76a22cb41a798e86764bba
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-11-10 10:16:31 +0100

    chore

--- modules/vlc-player/android/src/main/java/expo/modules/vlcplayer/VlcPlayerView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.vlcplayer
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class VlcPlayerView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
