5afb677b3ac7062cdafd68fc2bee077a7b9cf0b0
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-12-08 11:57:47 +0100

    chore

--- utils/profiles/native.js
+++ utils/profiles/native.js
@@ -10,12 +10,21 @@ import MediaTypes from "../../constants/MediaTypes";
  */
 export default {
   Name: "1. Vlc Player",
-  MaxStaticBitrate: 20_000_000,
-  MaxStreamingBitrate: 20_000_000,
+  MaxStaticBitrate: 9999_000_000,
+  MaxStreamingBitrate: 9999_000_000,
   CodecProfiles: [
     {
       Type: MediaTypes.Video,
       Codec: "h264,h265,hevc,mpeg4,divx,xvid,wmv,vc1,vp8,vp9,av1",
+      // TODO: Fix dolby vision playback
+      // Conditions: [
+      //   {
+      //     Condition: "NotEquals",
+      //     Property: "VideoProfile",
+      //     Value: "dolby-vision|dvhe|dvh1|dvhe.05|dvhe.07",
+      //     IsRequired: false,
+      //   },
+      // ],
     },
     {
       Type: MediaTypes.Audio,
